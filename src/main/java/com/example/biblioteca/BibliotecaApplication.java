16569feec0c0fd38e03af3e0ab082aad4dd6dd2d
andrefernandeslp1 <andre.mdia@gmail.com>
2024-04-06 21:00:26 -0300

    .

--- src/main/java/com/example/biblioteca/BibliotecaApplication.java
+++ src/main/java/com/example/biblioteca/BibliotecaApplication.java
@@ -55,52 +55,71 @@ public class BibliotecaApplication {
       Autor autor2 = new Autor("Shakespeare", "Reino Unido");
       Autor autor3 = new Autor("Machado de Assis", "Brasil");
 
-      Livro livro1 = new Livro("It", "Terror", "1986", editora1);
-      Livro livro2 = new Livro("Hamlet", "Drama", "1600", editora2);
-      Livro livro3 = new Livro("Dom Casmurro", "Romance", "1899", editora3);
-      
-      List<Livro> livros1 = new ArrayList<>();
-      livros1.add(livro1);
-      List<Livro> livros2 = new ArrayList<>();
-      livros2.add(livro2);
-      List<Livro> livros3 = new ArrayList<>();
-      livros3.add(livro3);
-
       autorRepository.save(autor1);
       autorRepository.save(autor2);
       autorRepository.save(autor3);
 
+      Livro livro1 = new Livro("It", "Terror", "1986", editora1);
+      Livro livro2 = new Livro("Hamlet", "Drama", "1600", editora2);
+      Livro livro3 = new Livro("Dom Casmurro", "Romance", "1899", editora3);
+      Livro livro4 = new Livro("O Iluminado", "Terror", "1977", editora1);
+      Livro livro5 = new Livro("Macbeth", "Drama", "1606", editora2);
+      Livro livro6 = new Livro("Memórias Póstumas de Brás Cubas", "Romance", "1881", editora3);
+      
+      List<Livro> livros1 = new ArrayList<>();
+      List<Livro> livros2 = new ArrayList<>();
+      List<Livro> livros3 = new ArrayList<>();
+      livros1.add(livro1);
+      livros2.add(livro2);
+      livros3.add(livro3);
+      livros1.add(livro4);
+      livros2.add(livro5);
+      livros3.add(livro6);
+
       List<Autor> autores1 = new ArrayList<>();
-      autores1.add(autor1);
       List<Autor> autores2 = new ArrayList<>();
-      autores2.add(autor2);
       List<Autor> autores3 = new ArrayList<>();
+      autores1.add(autor1);
+      autores2.add(autor2);
       autores3.add(autor3);
 
       livro1.setAutores(autores1);
       livro2.setAutores(autores2);
       livro3.setAutores(autores3);
+      livro4.setAutores(autores1);
+      livro5.setAutores(autores2);
+      livro6.setAutores(autores3);
 
       livroRepository.save(livro1);
       livroRepository.save(livro2);
       livroRepository.save(livro3);
+      livroRepository.save(livro4);
+      livroRepository.save(livro5);
+      livroRepository.save(livro6);
 
       Usuario usuario1 = new Usuario("João", "joao@email.com");
       Usuario usuario2 = new Usuario("Maria", "maria@email.com");
       Usuario usuario3 = new Usuario("José", "jose@email.com");
+      Usuario usuario4 = new Usuario("Ana", "ana@email.com");
+      Usuario usuario5 = new Usuario("Pedro", "pedro@email.com");
+      Usuario usuario6 = new Usuario("Carla", "carla@email.com");
 
       usuarioRepository.save(usuario1);
       usuarioRepository.save(usuario2);
       usuarioRepository.save(usuario3);
+      usuarioRepository.save(usuario4);
+      usuarioRepository.save(usuario5);
+      usuarioRepository.save(usuario6);
 
       Emprestimo emprestimo1 = new Emprestimo(usuario1, livro1);
+      Emprestimo emprestimo2 = new Emprestimo(usuario2, livro2);
+      Emprestimo emprestimo3 = new Emprestimo(usuario3, livro3);
 
       emprestimoRepository.save(emprestimo1);
+      emprestimoRepository.save(emprestimo2);
+      emprestimoRepository.save(emprestimo3);
 
-
-
-
-
+      
     };
 
   }
